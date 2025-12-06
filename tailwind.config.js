5243e8697bd29b3b927e3dd2df833d5e1c6ed738
salhed <economist.sed@gmail.com>
2025-12-06 10:32:40 +0300

    add tailwind files

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+// tailwind.config.js
+module.exports = {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}", // Tüm src altındaki React dosyalarını dahil et
+    "./public/index.html",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
